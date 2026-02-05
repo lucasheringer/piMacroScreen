84f2a9d5e662fd4e30e6a2e5ed65ecd7962c04d7
Lucas Heringer <16108409+lucasheringer@users.noreply.github.com>
2026-02-05 18:13:46 +0000

    more stuff

--- usbHidKeyboard.py
+++ usbHidKeyboard.py
@@ -33,9 +33,9 @@ KEYS_ALLOWED = {
   'PAUSE_UNPAUSE':  {'rsvd': 8, 'ctrl': CONTROL_CODE, 'kbd': 0x10, 'delay': 0.1, 'ctrl': CONTROL_CODE},
   'CMD_SHIFT_M':    {'rsvd': 0, 'ctrl': (CMD_CODE | 0x02), 'kbd': 0x10, 'delay': 0.1},
   'TEST_A':         {'rsvd': 0, 'ctrl': 0x00, 'kbd': 0x04, 'delay': 0.1},
-  'TEST_SHIFT_M':   {'rsvd': 0, 'ctrl': CONTROL_CODE, 'kbd': 0x10, 'delay': 0.1},
+  'TEST_SHIFT_M':   {'rsvd': 0, 'ctrl': 0x04, 'kbd': 0x10, 'delay': 0.1},
   'TEST_CMD_A':     {'rsvd': 0, 'ctrl': 0x08, 'kbd': 0x04, 'delay': 0.1},
-  'TEST_SHIFT_A':   {'rsvd': 0, 'ctrl': CONTROL_CODE, 'kbd': 0x04, 'delay': 0.1},
+  'TEST_SHIFT_A':   {'rsvd': 0, 'ctrl': 0x04, 'kbd': 0x04, 'delay': 0.1},
 }
 
 # Helper script to send data directly to our HID Gadget emulation device
