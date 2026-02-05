93ff0737b1cabf0abf6160ce4e479b9057b8e4a6
Lucas Heringer <16108409+lucasheringer@users.noreply.github.com>
2026-02-05 14:07:02 +0000

    more stuff

--- macroKeys.py
+++ macroKeys.py
@@ -1,7 +1,27 @@
 import os
 import pygame
+
+# Define basic colors
+SHADOW = (192, 192, 192)
+WHITE = (255, 255, 255)
+LIGHTGREEN = (0, 255, 0 )
+GREEN = (0, 200, 0 )
+BLUE = (0, 0, 128)
+LIGHTBLUE = (0, 0, 255)
+RED = (200, 0, 0 )
+LIGHTRED = (255, 100, 100)
+PURPLE = (102, 0, 102)
+LIGHTPURPLE = (153, 0, 153)
+BLACK = (0, 0, 0)
+
 screen = pygame.display.set_mode((320, 240))
 clock = pygame.time.Clock()
+
+os.putenv('SDL_FBDEV', '/dev/fb0')
+# os.putenv('SDL_MOUSEDRV', 'TSLIB')
+os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
+screen.display.set_caption('Macro Keys')
+screen.mouse.set_visible(False)
 
 for x in range(20):
     screen.fill((0, 0, 0))
