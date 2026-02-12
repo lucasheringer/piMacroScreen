8202c478de6d828238fcf60da164f4bae8f34f5e
Lucas Heringer <16108409+lucasheringer@users.noreply.github.com>
2026-02-12 17:22:27 +0000

    more stuff

--- macroKeys.py
+++ macroKeys.py
@@ -182,7 +182,8 @@ for row in range(BUTTON_ROWS):
 # Function to draw all buttons
 def drawButtons():
     # lcd.fill((255, 255, 255))  # Background
-    lcd.blit(bg_image, (0, 0))
+    if bg_image is not None:
+        lcd.blit(bg_image, (0, 0))
     for btn in buttons:
         pygame.draw.rect(lcd, btn['color'], btn['rect'], 3)  # Draw border
         # Draw icon or button number in center
